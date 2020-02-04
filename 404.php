cdb780246bff880454b2124c790e8a32ee92c40c
deadie <zaq12wsx>
2020-02-04 09:17:37 +0300

    [upd] 404

--- 404.php
+++ 404.php
@@ -10,7 +10,19 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("404 Not Found");
 ?>
 
+<div class="error-404">
+    <div class="container">
+        <div class="row">
+            <div class="error-404--wrapper col-12">
+                <h1>404</h1>
+                <p><em>Этой страницы<br>не существует</em></p>
+                <p>
+                    <a href="/" class="btn btn-white">Вернуться на главную</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 
-<?php
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
