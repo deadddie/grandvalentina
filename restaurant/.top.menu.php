8dd3a93b4ab9f325fb5ca1d1458ab8237c2c3069
deadie <zaq12wsx>
2020-02-14 00:19:42 +0300

    [upd] top menu страницы Ресторан

---
+++ restaurant/.top.menu.php
@@ -0,0 +1,38 @@
+<?php
+$aMenuLinks = array(
+	array(
+		"Банкетный зал",
+		"restaurants/banquet/",
+		Array(),
+		Array(),
+		""
+	),
+    array(
+        "Ресторан «Freesia»",
+        "restaurants/freesie/",
+        Array(),
+        Array(),
+        ""
+    ),
+    array(
+        "Кейтеринг",
+        "restaurants/catering/",
+        Array(),
+        Array(),
+        ""
+    ),
+    array(
+        "Бортовое питание",
+        "restaurants/onboard/",
+        Array(),
+        Array(),
+        ""
+    ),
+    array(
+        "Лобби-бар",
+        "restaurants/lobby-bar/",
+        Array(),
+        Array(),
+        ""
+    ),
+);
