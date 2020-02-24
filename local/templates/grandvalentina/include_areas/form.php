b914902b42a4c374cb0f4fbccd8facbc6b327cf7
deadie <zaq12wsx>
2020-02-24 05:27:11 +0300

    [upd] формы добавлен опциональный разделитель под заголовок, типы переменных

--- local/templates/grandvalentina/include_areas/form.php
+++ local/templates/grandvalentina/include_areas/form.php
@@ -3,16 +3,18 @@
 /**
  * Default form.
  *
- * @var $id
- * @var $title
- * @var $description
- * @var $submit_button
- * @var $fields
- * @var $required
- * @var $wrapper
+ * @var string $id
+ * @var string $title
+ * @var string $description
+ * @var string $submit_button
+ * @var array $fields
+ * @var array $required
+ * @var bool $wrapper
+ * @var bool $title_delimiter
  */
 
 use Bitrix\Main\Security\Random;
+use LapkinLab\Core;
 
 $form_id = $id . '-form-' . Random::getString(32);
 ?>
@@ -24,6 +26,9 @@ $form_id = $id . '-form-' . Random::getString(32);
     <input type="hidden" name="url" value="https://<?= $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] ?>">
     <?php if ($wrapper): ?><div class="form--wrapper"><?php endif; ?>
         <h2 class="form--title text-center"><?= $title ?></h2>
+        <?php if ($title_delimiter): ?>
+            <div class="form--title--delimiter delimiter"><?= Core::renderIcon('delimiter') ?></div>
+        <?php endif; ?>
         <div class="form--description text-center"><?= $description ?></div>
         <div class="form--fields">
             <?php foreach ($fields as $field) {
