13459b147180723c2c2280ae8093e2165c7d12b4
deadie <zaq12wsx>
2020-03-14 14:43:00 +0300

    [add] кнопка "Подобрать номер" на главной

--- local/templates/grandvalentina/include_areas/front_screen.php
+++ local/templates/grandvalentina/include_areas/front_screen.php
@@ -4,5 +4,8 @@
     <div class="front-screen--slogan"><?= SITE_CONFIG['slogan'] ?></div>
     <div class="front-screen--scroller mouse-scroller"><?= renderIcon('mouse') ?></div>
 </section>
+<div class="front-screen--booking">
+    <a href="/booking/" class="btn btn-wide">Подобрать номер</a>
+</div>
 
 
