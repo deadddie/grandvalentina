726f27a42f92b7ee5298f8ba0413a486c0369bf1
deadie <zaq12wsx>
2020-03-24 09:28:14 +0300

    [upd] скрипт в бронировании

--- booking/index.php
+++ booking/index.php
@@ -32,20 +32,21 @@ $APPLICATION->SetPageProperty('title', $title);
                     <!-- start TL Booking form script -->
                     <div id="tl-booking-form">&nbsp;</div>
                     <script>
-                        // Roistat Begin
-                        function bookingSuccess(data) {
-                            $.get('//grandvalentina.ru/roistat/webhooks/armor.php', data);
-                        }
-                        // Roistat End
-
-                        (function(w){
-                            var q=[
-                                ['setContext', 'TL-INT-grandvalentina', 'ru'],
-                                ['embed', 'booking-form', {container: 'tl-booking-form'}]
-                            ];
-                            var t=w.travelline=(w.travelline||{}),ti=t.integration=(t.integration||{});ti.__cq=ti.__cq?ti.__cq.concat(q):q;
-                            if (!ti.__loader){ti.__loader=true;var d=w.document,p=d.location.protocol,s=d.createElement('script');s.type='text/javascript';s.async=true;s.src=(p=='https:'?p:'http:')+'//ibe.tlintegration.com/integration/loader.js';(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(s);}
-                        })(window);
+                        $(document).ready(function () {
+                            // Roistat Begin
+                            function bookingSuccess(data) {
+                                $.get('//grandvalentina.ru/roistat/webhooks/armor.php', data);
+                            }
+                            // Roistat End
+                            (function(w){
+                                var q=[
+                                    ['setContext', 'TL-INT-grandvalentina', 'ru'],
+                                    ['embed', 'booking-form', {container: 'tl-booking-form'}]
+                                ];
+                                var t=w.travelline=(w.travelline||{}),ti=t.integration=(t.integration||{});ti.__cq=ti.__cq?ti.__cq.concat(q):q;
+                                if (!ti.__loader){ti.__loader=true;var d=w.document,p=d.location.protocol,s=d.createElement('script');s.type='text/javascript';s.async=true;s.src=(p=='https:'?p:'http:')+'//ibe.tlintegration.com/integration/loader.js';(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(s);}
+                            })(window);
+                        });
                     </script>
                     <!-- end TL Booking form script -->
 
