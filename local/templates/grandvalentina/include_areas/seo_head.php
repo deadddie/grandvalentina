e2961ffda09dc613f458b05d75f39d0b50ba3262
deadie <zaq12wsx>
2020-03-24 09:29:56 +0300

    [fix] seo-скрипты

--- local/templates/grandvalentina/include_areas/seo_head.php
+++ local/templates/grandvalentina/include_areas/seo_head.php
@@ -1,4 +1,4 @@
-<script>
+<script data-skip-moving="true">
     // Roistat Begin
     function bookingSuccess(data) {
         $.get('//grandvalentina.ru/roistat/webhooks/armor.php', data);
@@ -7,11 +7,11 @@
 </script>
 
 <!--Pixel VK-->
-<script>!function(){var t=document.createElement("script");t.async=!0,t.src="https://vk.com/js/api/openapi.js?162",t.onload=function(){VK.Retargeting.Init("VK-RTRG-397277-cxrNL"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-397277-cxrNL" style="position:fixed; left:-999px;" alt=""/></noscript>
+<script data-skip-moving="true">!function(){var t=document.createElement("script");t.async=!0,t.src="https://vk.com/js/api/openapi.js?162",t.onload=function(){VK.Retargeting.Init("VK-RTRG-397277-cxrNL"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-397277-cxrNL" style="position:fixed; left:-999px;" alt=""/></noscript>
 <!--end pixel vk-->
 
 <!-- Facebook Pixel Code -->
-<script>
+<script data-skip-moving="true">
     !function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
